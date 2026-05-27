b0bd1f7078539b5dda8bb8784f96e87fac0909f2
Bhagavan GopalaJosyula <134036879+bhagavan444@users.noreply.github.com>
2026-05-27 21:51:49 +0530

    Fix ASGI app for gunicorn

--- backend/flask_app.py
+++ backend/flask_app.py
@@ -3,9 +3,8 @@ import traceback
 
 def create_app():
     try:
-        from a2wsgi import ASGIMiddleware
         from app.main import app as fastapi_app
-        return ASGIMiddleware(fastapi_app)
+        return fastapi_app
     except Exception as e:
         print("CRITICAL ERROR BOOTING APP:", e, file=sys.stderr)
         traceback.print_exc(file=sys.stderr)
